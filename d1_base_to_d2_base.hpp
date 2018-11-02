5cb425cbdabd2867e78cc602450805bbcfb5af10
Yaroslav Belgorosky <yshchukin@yandex.ru>
2018-11-02 22:50:26 +0300

    1.2: D1BaseToD2Base solution

---
+++ d1_base_to_d2_base.hpp
@@ -0,0 +1,7 @@
+#pragma once
+// base указывает на экземпляр Base, соответствующий D1
+// нужно вернуть указатель на экземпляр Base, соответствующий D2
+Base const * D1BaseToD2Base( Base const * base )
+{
+    return static_cast<D2 const *>(static_cast<D3 const *>(static_cast<D1 const *>(base)));
+}
