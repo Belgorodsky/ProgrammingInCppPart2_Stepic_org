2b2e724b70b98a3a7008fce63e6296d05cf92edd
Yaroslav Belgorosky <yshchukin@yandex.ru>
2018-11-11 17:24:01 +0300

    4.3: do_math solution

---
+++ 04/do_math.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <utility> // std::declval
+
+// внутри do_math объекты типа T
+// - копируются
+// - присваиваются
+// - складываются оператором +
+template<class T>
+void do_math() noexcept(
+    std::is_nothrow_copy_constructible<T>::value &&
+    std::is_nothrow_copy_assignable<T>::value &&
+    noexcept(std::declval<T>() + std::declval<T>()) 
+)
+{
+    // тело функции нужно оставить пустым
+}
