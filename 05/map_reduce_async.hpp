c3d1ca344b7bee8465359deaa583da50de6e984b
Yaroslav Belgorosky <yshchukin@yandex.ru>
2018-11-13 20:36:48 +0300

    5.1: map_reduce_async solution was refactored

--- 05/map_reduce_async.hpp
+++ 05/map_reduce_async.hpp
@@ -1,24 +1,43 @@
 #pragma once
-#include <future>
-#include <functional>
-#include <list>
+// заголовок <future> уже подключён.
+// заново подключать не нужно
+
+// реализация функции mapreduce
 #include <vector>
-#include <cassert>
-#include <cmath>
 #include <numeric>
-#include <type_traits>
 #include <algorithm>
 
+
 template<class InputIt, class UnaryFunctor, class BinaryFunctor>
-auto map_reduce_chunk(InputIt begin, InputIt end, UnaryFunctor f1, BinaryFunctor f2) -> decltype(f2(f1(*begin), f1(*begin))) 
+inline auto map_reduce_chunk_impl(InputIt begin, InputIt end, UnaryFunctor f1, BinaryFunctor f2, std::integral_constant<bool, true>)
+    -> decltype(f2(f1(*begin), f1(*begin))) 
+{
+	std::transform(begin, end, begin, f1);
+	return std::accumulate(std::next(begin), end, *begin, f2);
+}
+
+
+template<class InputIt, class UnaryFunctor, class BinaryFunctor>
+inline auto map_reduce_chunk_impl(InputIt begin, InputIt end, UnaryFunctor f1, BinaryFunctor f2, std::integral_constant<bool, false>)
+    -> decltype(f2(f1(*begin), f1(*begin))) 
 {
 	std::vector<decltype(f1(*begin))> mappings;
 	std::transform(begin, end, std::back_inserter(mappings), f1);
 	return std::accumulate(mappings.begin() + 1, mappings.end(), mappings.front(), f2);
 }
 
+
 template<class InputIt, class UnaryFunctor, class BinaryFunctor>
-auto map_reduce(InputIt begin, InputIt end, UnaryFunctor f1, BinaryFunctor f2, size_t num) -> decltype(f2(f1(*begin), f1(*begin))) 
+inline auto map_reduce_chunk(InputIt begin, InputIt end, UnaryFunctor f1, BinaryFunctor f2)
+    -> decltype(f2(f1(*begin), f1(*begin))) 
+{
+    return map_reduce_chunk_impl(begin, end, f1, f2, std::is_same<decltype(f1(*begin)),decltype(*begin)>{});
+}
+
+
+template<class InputIt, class UnaryFunctor, class BinaryFunctor>
+auto map_reduce(InputIt begin, InputIt end, UnaryFunctor f1, BinaryFunctor f2, size_t num)
+    -> decltype(f2(f1(*begin), f1(*begin))) 
 {
 	using future_t = std::future<decltype(f2(f1(*begin), f1(*begin))) >;
 	std::vector<future_t> threads;
@@ -26,17 +45,27 @@ auto map_reduce(InputIt begin, InputIt end, UnaryFunctor f1, BinaryFunctor f2, s
 	auto d = std::distance(begin, end);
 	auto len = d / num;
 	auto add = d % num;
-	for(size_t i = 0; i < num; ++i) {
+	while(num--)
+    {
 		auto till = begin;
-		auto l = len;
-		if (add > 0) {
-			++l;
-			--add;
-		}
-		std::advance(till, l);
-		threads.push_back(std::async(std::launch::async, map_reduce_chunk<InputIt, UnaryFunctor, BinaryFunctor>, begin, till, f1, f2));
+		std::advance(till, num ? len : len + add);
+		threads.push_back(
+            std::async(
+                std::launch::async,
+                map_reduce_chunk<InputIt, UnaryFunctor, BinaryFunctor>,
+                begin, 
+                till, 
+                f1, 
+                f2
+            )
+        );
 		begin = till;
 	}
 
-	return map_reduce_chunk(threads.begin(), threads.end(), [](future_t& f) { return f.get(); }, f2);
+	return map_reduce_chunk(
+        threads.begin(),
+        threads.end(),
+        [](future_t& f) { return f.get(); },
+        f2
+    );
 }
